640391b535b0612c860c1ec4f20a8abda6297219
QianJiu05 <3211471056@qq.com>
2025-12-04 11:18:04 +0000

    [mod]move global sema into syscall_init

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,7 +13,6 @@
 #include "devices/input.h"
 
 struct semaphore global;
-bool sema_is_init = false;
 static void syscall_handler(struct intr_frame*);
 
 static void check_valid_num(uint32_t* args);
@@ -21,8 +20,201 @@ static void check_valid_str(const char* str);
 static void check_valid_buffer(const void* buffer, size_t size);
 
 
-void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
+void syscall_init(void) { 
+    intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); 
+    sema_init(&global,1);
+}
 
+static void syscall_exit(int status);
+static uint32_t syscall_exec(const char* file_name);
+static bool syscall_create(const char *file, unsigned initial_size);
+static bool syscall_remove(const char *file);
+static int syscall_open(const char *file);
+static int syscall_filesize(int fd);
+static int syscall_read(int fd, void* buffer, unsigned size);
+static uint32_t syscall_wait(pid_t pid);
+static int syscall_write(int fd, void* buffer, size_t size);
+static void syscall_seek(int fd,unsigned position);
+static int syscall_tell(int fd);
+static void syscall_close(int fd);
+static pid_t syscall_fork(struct intr_frame* f);
+static tid_t syscall_pt_create(stub_fun sfun, pthread_fun tfun, const void* arg);
+static void syscall_pt_exit(void);
+static tid_t syscall_pt_join(tid_t tid);
+
+
+//arg[0]是调用号，其余是参数
+static void syscall_handler(struct intr_frame* f UNUSED) {
+    //调用者的堆栈指针可以通过传递给它的 struct intr_frame 的 esp 成员访问。指针数组
+    uint32_t *args = ((uint32_t*)f->esp);//32bit width
+
+    //   printf("arg0 = %d, arg2 = %s\n",args[0],(char*)args[2]);
+    check_valid_num(&args[0]);//检查栈顶指针是否有问题
+
+    switch(args[0]){
+        case SYS_HALT:
+            shutdown_power_off();
+            break;
+            
+        case SYS_EXIT:
+            check_valid_num(&args[1]);
+            syscall_exit(args[1]);
+            break;
+        
+        case SYS_EXEC:
+            check_valid_str((char*)args[1]);
+            f->eax = syscall_exec((char*)args[1]);
+            break;
+        
+        case SYS_WAIT:
+            check_valid_num(&args[1]);
+            f->eax = syscall_wait(args[1]);
+            break;
+        
+        case SYS_CREATE:
+            check_valid_str((char*)args[1]);
+            check_valid_num(&args[2]);
+            f->eax = syscall_create((char*)args[1],args[2]);
+            break;
+        
+        case SYS_REMOVE:
+            check_valid_str((char*)args[1]);
+            f->eax = syscall_remove((char*)args[1]);
+            break;
+        
+        case SYS_OPEN:
+            check_valid_str((char*)args[1]);
+            f->eax = syscall_open((char*)args[1]);
+            break;
+
+        case SYS_FILESIZE:
+            check_valid_num(&args[1]);
+            f->eax = syscall_filesize(args[1]);
+            break;
+        
+        case SYS_READ:
+            check_valid_num(&args[1]);
+            check_valid_num(&args[3]);
+            check_valid_buffer((void*)args[2],args[3]);
+            f->eax = syscall_read(args[1],(void*)args[2],args[3]);
+            break;
+
+        case SYS_WRITE:
+            check_valid_num(&args[1]);
+            check_valid_num(&args[3]);
+            check_valid_buffer((void*)args[2],args[3]);
+            f->eax = syscall_write((int)args[1],(void*)args[2],(size_t)args[3]);
+            break;
+
+        case SYS_SEEK:
+            check_valid_num(&args[1]);
+            check_valid_num(&args[2]);
+            syscall_seek(args[1],args[2]);
+            break;
+
+        case SYS_TELL:
+            check_valid_num(&args[1]);
+            f->eax = syscall_tell(args[1]);
+            break;
+
+        case SYS_CLOSE:
+            check_valid_num(&args[1]);
+            syscall_close(args[1]);
+            break;
+        
+        case SYS_PRACTICE:
+            check_valid_num(&args[1]);
+            f->eax = args[1] + 1;
+            break;
+
+        case SYS_FORK:
+            f->eax = syscall_fork(f);
+            break;
+
+        case SYS_PT_CREATE:
+            f->eax = syscall_pt_create((stub_fun)args[1],(pthread_fun)args[2],(void*)args[3]);
+            break;
+        
+        case SYS_PT_EXIT:
+            syscall_pt_exit();
+            break;
+
+        case SYS_PT_JOIN:
+            syscall_pt_join(args[1]);
+            break;
+
+    }
+}
+
+/* 验证num是否在用户空间、指针指向的地址是否是已分配内存的 */
+static void check_valid_num(uint32_t* num){
+    struct thread *t = thread_current();
+
+    if(num == NULL || pagedir_get_page(t->pcb->pagedir,num) == NULL)
+    {//pgdir_getpage已经检查了是否在uaddr
+        syscall_exit(-1);
+    }
+    void* this_byte = (void*)num;
+    for(int i = 1; i <= 3; i++){
+        this_byte = (void*)((char*)this_byte + i);
+        if(num == NULL || pagedir_get_page(t->pcb->pagedir,this_byte) == NULL)
+        {//pgdir_getpage已经检查了是否在uaddr
+            syscall_exit(-1);
+        }
+    }
+}
+static void check_valid_str(const char* str){
+    struct thread *t = thread_current();
+    if(str == NULL){
+        syscall_exit(-1);
+    }
+    if(pagedir_get_page(t->pcb->pagedir,str) == NULL){
+        syscall_exit(-1);
+    }
+
+    char* p = str;
+    int cnt = 0;
+    
+    while(cnt < 1024){
+        if(pagedir_get_page(t->pcb->pagedir,p) == NULL){
+            syscall_exit(-1);
+        }
+        if(*p == '\0')return;//找到结尾了，不用继续了
+        p++;
+        cnt++;
+    }
+    if(cnt >= 1024){
+        syscall_exit(-1);
+    }
+}
+static void check_valid_buffer(const void* buffer, size_t size){
+    if(buffer == NULL){
+        syscall_exit(-1);
+    }
+
+    if(size == 0)return;
+
+    struct thread *t = thread_current();
+
+    if(pagedir_get_page(t->pcb->pagedir,buffer) == NULL){
+        syscall_exit(-1);
+    }
+
+    void* end_buffer = (void*)(char*)buffer + size - 1;//指向最后一个字节
+
+    void* start = pg_round_down(buffer);
+    void* end = pg_round_down(end_buffer);
+
+    if(start != end){
+        void* page = (void*)(char*)start + PGSIZE;
+        while(page <= end){
+            if(pagedir_get_page(t->pcb->pagedir,page) == NULL){
+                syscall_exit(-1);
+            }
+            page = (void*)(char*)page + PGSIZE;
+        }
+    }    
+}
 static void syscall_exit(int status){
     printf("%s: exit(%d)\n", thread_current()->pcb->process_name, status);
 
@@ -219,181 +411,4 @@ static void syscall_pt_exit(void){
 }
 static tid_t syscall_pt_join(tid_t tid){
     return pthread_join(tid);
-}
-//arg[0]是调用号，其余是参数
-static void syscall_handler(struct intr_frame* f UNUSED) {
-    //调用者的堆栈指针可以通过传递给它的 struct intr_frame 的 esp 成员访问。指针数组
-    uint32_t *args = ((uint32_t*)f->esp);//32bit width
-
-    if(sema_is_init == false){
-        sema_init(&global,1);
-        sema_is_init = true;
-    }
-
-    //   printf("arg0 = %d, arg2 = %s\n",args[0],(char*)args[2]);
-    check_valid_num(&args[0]);//检查栈顶指针是否有问题
-
-    switch(args[0]){
-        case SYS_HALT:
-            shutdown_power_off();
-            break;
-            
-        case SYS_EXIT:
-            check_valid_num(&args[1]);
-            syscall_exit(args[1]);
-            break;
-        
-        case SYS_EXEC:
-            check_valid_str((char*)args[1]);
-            f->eax = syscall_exec((char*)args[1]);
-            break;
-        
-        case SYS_WAIT:
-            check_valid_num(&args[1]);
-            f->eax = syscall_wait(args[1]);
-            break;
-        
-        case SYS_CREATE:
-            check_valid_str((char*)args[1]);
-            check_valid_num(&args[2]);
-            f->eax = syscall_create((char*)args[1],args[2]);
-            break;
-        
-        case SYS_REMOVE:
-            check_valid_str((char*)args[1]);
-            f->eax = syscall_remove((char*)args[1]);
-            break;
-        
-        case SYS_OPEN:
-            check_valid_str((char*)args[1]);
-            f->eax = syscall_open((char*)args[1]);
-            break;
-
-        case SYS_FILESIZE:
-            check_valid_num(&args[1]);
-            f->eax = syscall_filesize(args[1]);
-            break;
-        
-        case SYS_READ:
-            check_valid_num(&args[1]);
-            check_valid_num(&args[3]);
-            check_valid_buffer((void*)args[2],args[3]);
-            f->eax = syscall_read(args[1],(void*)args[2],args[3]);
-            break;
-
-        case SYS_WRITE:
-            check_valid_num(&args[1]);
-            check_valid_num(&args[3]);
-            check_valid_buffer((void*)args[2],args[3]);
-            f->eax = syscall_write((int)args[1],(void*)args[2],(size_t)args[3]);
-            break;
-
-        case SYS_SEEK:
-            check_valid_num(&args[1]);
-            check_valid_num(&args[2]);
-            syscall_seek(args[1],args[2]);
-            break;
-
-        case SYS_TELL:
-            check_valid_num(&args[1]);
-            f->eax = syscall_tell(args[1]);
-            break;
-
-        case SYS_CLOSE:
-            check_valid_num(&args[1]);
-            syscall_close(args[1]);
-            break;
-        
-        case SYS_PRACTICE:
-            check_valid_num(&args[1]);
-            f->eax = args[1] + 1;
-            break;
-
-        case SYS_FORK:
-            f->eax = syscall_fork(f);
-            break;
-
-        case SYS_PT_CREATE:
-            f->eax = syscall_pt_create((stub_fun)args[1],(pthread_fun)args[2],(void*)args[3]);
-            break;
-        
-        case SYS_PT_EXIT:
-            syscall_pt_exit();
-            break;
-
-        case SYS_PT_JOIN:
-            syscall_pt_join(args[1]);
-            break;
-
-    }
-}
-
-/* 验证num是否在用户空间、指针指向的地址是否是已分配内存的 */
-static void check_valid_num(uint32_t* num){
-    struct thread *t = thread_current();
-
-    if(num == NULL || pagedir_get_page(t->pcb->pagedir,num) == NULL)
-    {//pgdir_getpage已经检查了是否在uaddr
-        syscall_exit(-1);
-    }
-    void* this_byte = (void*)num;
-    for(int i = 1; i <= 3; i++){
-        this_byte = (void*)((char*)this_byte + i);
-        if(num == NULL || pagedir_get_page(t->pcb->pagedir,this_byte) == NULL)
-        {//pgdir_getpage已经检查了是否在uaddr
-            syscall_exit(-1);
-        }
-    }
-}
-static void check_valid_str(const char* str){
-    struct thread *t = thread_current();
-    if(str == NULL){
-        syscall_exit(-1);
-    }
-    if(pagedir_get_page(t->pcb->pagedir,str) == NULL){
-        syscall_exit(-1);
-    }
-
-    char* p = str;
-    int cnt = 0;
-    
-    while(cnt < 1024){
-        if(pagedir_get_page(t->pcb->pagedir,p) == NULL){
-            syscall_exit(-1);
-        }
-        if(*p == '\0')return;//找到结尾了，不用继续了
-        p++;
-        cnt++;
-    }
-    if(cnt >= 1024){
-        syscall_exit(-1);
-    }
-}
-static void check_valid_buffer(const void* buffer, size_t size){
-    if(buffer == NULL){
-        syscall_exit(-1);
-    }
-
-    if(size == 0)return;
-
-    struct thread *t = thread_current();
-
-    if(pagedir_get_page(t->pcb->pagedir,buffer) == NULL){
-        syscall_exit(-1);
-    }
-
-    void* end_buffer = (void*)(char*)buffer + size - 1;//指向最后一个字节
-
-    void* start = pg_round_down(buffer);
-    void* end = pg_round_down(end_buffer);
-
-    if(start != end){
-        void* page = (void*)(char*)start + PGSIZE;
-        while(page <= end){
-            if(pagedir_get_page(t->pcb->pagedir,page) == NULL){
-                syscall_exit(-1);
-            }
-            page = (void*)(char*)page + PGSIZE;
-        }
-    }    
 }
