393b49eabc464674b2f70df8bc39085c573505ac
QianJiu05 <3211471056@qq.com>
2025-12-13 14:04:51 +0000

    [mod] change exception printf logic

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -98,19 +98,7 @@ static void kill(struct intr_frame* f) {
   }
 }
 
-/* Page fault handler.  This is a skeleton that must be filled in
-   to implement virtual memory.  Some solutions to project 2 may
-   also require modifying this code.
-
-   At entry, the address that faulted is in CR2 (Control Register
-   2) and information about the fault, formatted as described in
-   the PF_* macros in exception.h, is in F's error_code member.  The
-   example code here shows how to parse that information.  You
-   can find more information about both of these in the
-   description of "Interrupt 14--Page Fault Exception (#PF)" in
-   [IA32-v3a] section 5.15 "Exception and Interrupt Reference".
-
-   页面错误处理程序。这是一个框架，需要填充才能实现虚拟内存。
+/* 页面错误处理程序。这是一个框架，需要填充才能实现虚拟内存。
    项目 2 的某些解决方案可能也需要修改此代码。
    在入口处，发生错误的地址位于 CR2（控制寄存器 2）中，
    并且有关错误的信息（格式如 exception.h 中的 PF_* 宏所述）位于 F 的 error_code 成员中。
@@ -123,14 +111,7 @@ static void page_fault(struct intr_frame* f) {
   bool user;        /* True: access by user, false: access by kernel. */
   void* fault_addr; /* Fault address. */
 
-  /* Obtain faulting address, the virtual address that was
-     accessed to cause the fault.  It may point to code or to
-     data.  It is not necessarily the address of the instruction
-     that caused the fault (that's f->eip).
-     See [IA32-v2a] "MOV--Move to/from Control Registers" and
-     [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
-     (#PF)".
-     获取故障地址，即导致故障的被访问的虚拟地址。
+  /* 获取故障地址，即导致故障的被访问的虚拟地址。
      它可能指向代码或数据。
      它不一定是导致故障的指令的地址（即 f->eip）。
      参见 [IA32-v2a] “MOV——移入/移出控制寄存器” 和
@@ -149,27 +130,18 @@ static void page_fault(struct intr_frame* f) {
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers.
-
-     要实现虚拟内存，请删除函数体的其余部分，
+  /* 要实现虚拟内存，请删除函数体的其余部分，
      并将其替换为将 fault_addr 指向的页面加载到该页面的代码。
    */
 
-   /* 用户程序导致page fault，返回exit-1并退出进程*/
-   if(user){
-         printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
+  printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
          not_present ? "not present" : "rights violation", write ? "writing" : "reading",
          user ? "user" : "kernel");
-         
+   /* 用户程序导致page fault，返回exit-1并退出进程*/
+   if(user){     
          f->eax = -1;
          printf("%s: exit(%d)\n", thread_current()->pcb->process_name, -1);
-         process_exit();
+         // process_exit();
    }
-   /* 内核导致的page fault*/
-   printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
-            not_present ? "not present" : "rights violation", write ? "writing" : "reading",
-            user ? "user" : "kernel");
    kill(f);
 }
